cad98e8c92dacd77927654eab9cf64cabbcc37ff
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-10-28 18:30:29 +0800

    Override equal operator for Rational

--- models/Rational.py
+++ models/Rational.py
@@ -1,6 +1,14 @@
+from typing import Self
+
+
 class Rational:
     def __init__(self, n: int, d: int) -> None:
         self.num = n
         self.den = d
-        
+
+    def __eq__(self, other: Self) -> bool:
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError
+
+        return self.num == other.num and self.den == other.den        
 
