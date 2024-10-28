1011ec6443d0876be123448b7e3000d98a8a10a3
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-10-28 18:25:27 +0800

    Added method simplify to Rational

--- models/Rational.py
+++ models/Rational.py
@@ -2,5 +2,23 @@ class Rational:
     def __init__(self, n: int, d: int) -> None:
         self.num = n
         self.den = d
+
+        self.simplify()
+
+    def simplify(self) -> None:
+        if self.den < 0:
+            self.num *= -1
+            self.den *= -1
+
+        a, b = abs(self.num), self.den
+        while a != b:
+            if a > b:
+                a -= b
+            else:
+                b -= a
+        
+        self.num //= a
+        self.den //= b
+
         
 
